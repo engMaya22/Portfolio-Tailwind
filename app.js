e1906827a4f502c9c8ac061a7d89869a2b937fb7
maya <eng.maya.esmaeel1@gmail.com>
2024-04-01 09:08:08 -0700

    contact section

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const burgerBtn = document.getElementById('burger');
 const menuBar = document.getElementById('menu');
-
+const hLinks = document.querySelectorAll('.hLink');
 
 burgerBtn.addEventListener('click',()=>{
     menuBar.classList.toggle('hidden')
     burgerBtn.classList.toggle('bg-white')
 
 })
+hLinks.forEach(link=>{
+    link.addEventListener('click',()=>{
+        menuBar.classList.toggle('hidden')
+        burgerBtn.classList.toggle('bg-white')
+    
+    })
+
+})
+
+
